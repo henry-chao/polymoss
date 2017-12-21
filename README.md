db22a9cae656d42f0d203f7bc13a65e89bffb4ae
Henry Chao <henry.chao@outlook.com>
2017-12-21 10:37:14 -0500

    Added configuration management for different environment. Also integrated OAUTH with Canvas API.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # polymoss
+
+Make sure you have a config.ini file created and defined!
--- index.py
+++ index.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, url_for, request, redirect
+import configparser
+
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 @app.route("/")
 def index():
   return render_template('index.jade',
-    title = "PolyMOSS"
+    title = "PolyMOSS",
+    canvas_instance = config['Canvas']['canvas_instance'],
+    client_id = config['Canvas']['client_id'],
+    redirect_uri = config['Canvas']['redirect_uri'],
   )
 
+@app.route("/oauth")
+def ouath():
+  code = request.args.get('code')
+  token_req = {'grant_type':'authorization_code',
+    'client_id':config['Canvas']['client_id'],
+    'client_secret':config['Canvas']['client_secret'],
+    'redirect_uri':config['Canvas']['redirect_uri'],
+    'code':code
+    }
+  response = requests.post("https://{}/login/oauth2/token".format(config['Canvas']['canvas_instance']), json=token_req)
+
+  access_token = response.json()['access_token']
+
+  return render_template('selection.jade')
+}
