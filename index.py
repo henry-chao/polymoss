80c152b8697daee1262f148d6bee459468e36c91
Henry Chao <henry.chao@outlook.com>
2017-12-21 10:50:27 -0500

    Fixed formatting issue.

--- index.py
+++ index.py
@@ -13,21 +13,19 @@ def index():
     title = "PolyMOSS",
     canvas_instance = config['Canvas']['canvas_instance'],
     client_id = config['Canvas']['client_id'],
-    redirect_uri = config['Canvas']['redirect_uri'],
+    redirect_uri = config['Canvas']['redirect_uri']
   )
 
 @app.route("/oauth")
 def ouath():
-  code = request.args.get('code')
-  token_req = {'grant_type':'authorization_code',
-    'client_id':config['Canvas']['client_id'],
-    'client_secret':config['Canvas']['client_secret'],
-    'redirect_uri':config['Canvas']['redirect_uri'],
-    'code':code
-    }
-  response = requests.post("https://{}/login/oauth2/token".format(config['Canvas']['canvas_instance']), json=token_req)
+  token_req = {'grant_type': 'authorization_code',
+               'client_id': config['Canvas']['client_id'],
+               'client_secret': config['Canvas']['client_secret'],
+               'redirect_uri': config['Canvas']['redirect_uri'],
+               'code': request.args.get('code')
+  }
 
+  response = requests.post("https://{}/login/oauth2/token".format(config['Canvas']['canvas_instance']), json=token_req)
   access_token = response.json()['access_token']
 
   return render_template('selection.jade')
-}
